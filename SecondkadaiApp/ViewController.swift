9e122627e97eafeb5fe967116117e94f3da08add
ryoko.hoshi <ryoko.hoshi@gmail.com>
2022-04-09 08:50:01 +0900

    Initial Commit

---
+++ SecondkadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondkadaiApp
+//
+//  Created by 星涼子 on 2022/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
